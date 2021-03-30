c566b7d0dce48ce4f04e851e5b3a2e1d62391b5c
Yoshihisa Masaki <salem225@gmail.com>
2021-03-31 07:18:41 +0900

    GitHub api が返してくる JSON 用のあらかじめ設定された decoder を用意
    static property にすることによって一度だけ初期化される、それが繰り返し使われる。

---
+++ iOSEngineerCodeCheck/Extensions/JSONDecoder+Decoders.swift
@@ -0,0 +1,20 @@
+//
+//  JSONDecoder+Decoders.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Yoshihisa Masaki on 2021/03/31.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+extension JSONDecoder {
+
+    static let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+
+        return decoder
+    }()
+}
